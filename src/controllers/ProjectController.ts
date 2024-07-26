9ea0229fa5eb8c35ce6eb25616f75486fe55dc2a
efraindrummer42a <may.efrain@sycec.com.mx>
2024-07-26 13:08:26 -0600

    CRUD mongo node

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -1,10 +1,11 @@
 import type { Request, Response } from "express"
+import colors from 'colors'
 import Project from "../models/Project";
 
 export class ProjectController {
 
     static createProject = async(req: Request, res: Response) => {
-        console.log('createProject....')
+        console.log(colors.red.bold('createProject....'))
 
         const project = new Project(req.body)
         try {
@@ -15,12 +16,78 @@ export class ProjectController {
         }
     }
     
-    static getAllProject = async(req: Request, res: Response) => {
+    static getAllProjects = async(req: Request, res: Response) => {
+        console.log(colors.blue.bold('getAllProject...'));
+
         try {
             const projects = await Project.find({})
             res.json(projects);
         } catch (error) {
             console.log(error)
+        }
+    }
+
+    static getProjectById = async(req: Request, res: Response) => {
+        console.log(colors.yellow.bold('getProjectById...'))
+
+        const { id } = req.params;
+        console.log(id)
+
+        try {
+            const project = await Project.findById(id)
+
+            if(!project){
+                const error = new Error('Proyecto no encontrado')
+                return res.status(404).json({error: error.message });
+            }
+            res.json(project)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    
+    static updateProject = async(req: Request, res: Response) => {
+        console.log(colors.green.bold('updateProject...'))
+
+        const { id } = req.params;
+        console.log(id)
+
+        try {
+            const project = await Project.findByIdAndUpdate(id, req.body);
+
+            if(!project){
+                const error = new Error('Proyecto no encontrado')
+                return res.status(404).json({error: error.message });
+            }
+
+            const p = await project.save();
+            console.log(p)
+            res.send('Proyecto actualizado')
+
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    static deleteProject = async(req: Request, res: Response) => {
+        console.log(colors.yellow.bold('deleteProjectById...'))
+
+        const { id } = req.params;
+        console.log(id)
+
+        try {
+            const project = await Project.findById(id);
+
+            if(!project){
+                const error = new Error('Proyecto no encontrado')
+                return res.status(404).json({error: error.message });
+            }
+
+            await project.deleteOne()
+            res.send('Proyecto eliminado')
+        } catch (error) {
+            console.log(error);
         }
     }
 }
--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { ProjectController } from "../controllers/ProjectController";
-import { body } from "express-validator";
+import { body, param } from "express-validator";
 import { handleInputErrors } from "../middlewares/validation";
 
 const router = Router();
@@ -12,7 +12,27 @@ router.post('/',
     handleInputErrors,
     ProjectController.createProject,
 )
-router.get('/', ProjectController.getAllProject)
+router.get('/', ProjectController.getAllProjects)
 
+router.get('/:id',
+    param('id').isMongoId().withMessage('ID no valido'),
+    handleInputErrors,
+    ProjectController.getProjectById
+)
+
+router.put('/:id',
+    param('id').isMongoId().withMessage('ID no valido'),
+    body('projectName').notEmpty().withMessage('El nombre del proyecto es obligatorio'),
+    body('clientName').notEmpty().withMessage('El nombre del cliente es obligatorio'),
+    body('description').notEmpty().withMessage('La descripcion es obligatoria'),
+    handleInputErrors,
+    ProjectController.updateProject
+)
+
+router.delete('/:id',
+    param('id').isMongoId().withMessage('ID no valido'),
+    handleInputErrors,
+    ProjectController.deleteProject
+)
 
 export default router
